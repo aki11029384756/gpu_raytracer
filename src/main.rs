5ddd2daf4001ea62fc336430f61a31f123f1554d
Aki <aki.rogeberg@gmail.com>
2026-02-04 13:18:16 +0100

    init or smtth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,101 @@
+use std::sync::Arc;
+
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window
+};
+
+// This will store the state of our game
+pub struct State {
+    window: Arc<Window>,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self {
+            window,
+        })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {
+        // We'll do stuff here in the next tutorial
+    }
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+        // We'll do more stuff here in the next tutorial
+    }
+}
+
+pub struct App {
+    state: Option<State>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self {
+            state: None,
+        }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes();
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        // Use pollster to await the async function
+        self.state = Some(pollster::block_on(State::new(window)).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        let state = match &mut self.state {
+            Some(state) => state,
+            None => return,
+        };
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => {
+                state.render();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                KeyEvent {
+                    physical_key: PhysicalKey::Code(code),
+                    state: key_state,
+                    ..
+                },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+fn run() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::new()?;
+    let mut app = App::new();
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap();
 }
